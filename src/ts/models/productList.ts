7bd13b6698bea58047808f180c4291080906cf8f
h4zan <h4zan@hotmail.com>
2022-12-20 11:34:31 +0100

    added more products

--- src/ts/models/productList.ts
+++ src/ts/models/productList.ts
@@ -2,17 +2,252 @@ export let products: Product[] = [
   new Product(
     "Ring",
     899,
-    "https://images.pexels.com/photos/8891950/pexels-photo-8891950.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/14466168/pexels-photo-14466168.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
-    "Golden ring",
+    "Golden diamond ring",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/14466167/pexels-photo-14466167.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden diamond ring",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/14466164/pexels-photo-14466164.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden diamond ring",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/14466162/pexels-photo-14466162.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden diamond ring",
     "M"
   ),
   new Product(
     "Ring",
     899,
+    "https://images.pexels.com/photos/14466158/pexels-photo-14466158.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Silver diamond ring",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/14466157/pexels-photo-14466157.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Silver diamond ring",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111401/pexels-photo-14111401.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111400/pexels-photo-14111400.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111399/pexels-photo-14111399.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111397/pexels-photo-14111397.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Rosegold chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111396/pexels-photo-14111396.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Rosegold chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111395/pexels-photo-14111395.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Rosegold chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111394/pexels-photo-14111394.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111393/pexels-photo-14111393.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/14111392/pexels-photo-14111392.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/10983782/pexels-photo-10983782.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Rosegold chain with heartshape pendant",
+    "M"
+  ),
+
+  new Product(
+    "Earring",
+    899,
+    "https://images.pexels.com/photos/10983783/pexels-photo-10983783.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Diamond earrings",
+    "M"
+  ),
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/10983785/pexels-photo-10983785.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden chain with pendant",
+    "M"
+  ),
+
+  new Product(
+    "Earring",
+    899,
+    "https://images.pexels.com/photos/10983778/pexels-photo-10983778.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Diamond earrings",
+    "M"
+  ),
+
+  new Product(
+    "Necklace",
+    899,
+    "https://images.pexels.com/photos/10983780/pexels-photo-10983780.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Silver chain with pendant ",
+    "M"
+  ),
+
+  new Product(
+    "Earrings",
+    899,
+    "https://images.pexels.com/photos/10983781/pexels-photo-10983781.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Diamond earrings",
+    "M"
+  ),
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/10976653/pexels-photo-10976653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden diamond rings",
+    "M"
+  ),
+
+  new Product(
+    "Earring",
+    899,
+    "https://images.pexels.com/photos/10976654/pexels-photo-10976654.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden diamond earrings",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/10976655/pexels-photo-10976655.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Rosegold rings",
+    "M"
+  ),
+
+  new Product("Ring", 899, "", "Ring", "Golden ring", "M"),
+  new Product("Ring", 899, "", "Ring", "Golden ring", "M"),
+  new Product("Ring", 899, "", "Ring", "Golden ring", "M"),
+
+  new Product("Ring", 899, "", "Ring", "Golden ring", "M"),
+
+  new Product(
+    "Bracelet",
+    899,
+    "https://images.pexels.com/photos/8891954/pexels-photo-8891954.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Golden and diamond bracelet",
+    "M"
+  ),
+
+  new Product(
+    "Bracelet",
+    899,
+    "https://images.pexels.com/photos/8891956/pexels-photo-8891956.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Diamond bracelet",
+    "M"
+  ),
+
+  new Product(
+    "Ring",
+    899,
     "https://images.pexels.com/photos/8891950/pexels-photo-8891950.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "Ring",
-    "Golden ring",
+    "Multicolor, gold, rosegold and silver ring",
+    "M"
+  ),
+  new Product(
+    "Ring",
+    899,
+    "https://images.pexels.com/photos/8891959/pexels-photo-8891959.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "Ring",
+    "Silver diamond ring",
     "M"
   ),
 ];
