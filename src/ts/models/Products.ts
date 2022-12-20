8c1f34d338ae385ddedb7164f1a90551df569832
adamovd <adam.ov.danielsson@gmail.com>
2022-12-20 09:45:22 +0100

    added product class and createhtml

---
+++ src/ts/models/Products.ts
@@ -0,0 +1,10 @@
+class Product {
+  constructor(
+    public name: string,
+    public price: number,
+    public image: string,
+    public type: string,
+    public description: string,
+    public size: string
+  ) {}
+}
