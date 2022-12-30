2dcd55b0e59084d30fe067a98a0b51dd212cb83f
jennie Forsell <forselljennie@gmail.com>
2022-12-30 11:11:44 +0100

    filtrering klar på alla olika smycken och även lagt in en all-knapp, så man kommer tillbaka till alla produkter

--- src/ts/products.ts
+++ src/ts/products.ts
@@ -11,7 +11,7 @@ window.onload = () => {
   toggleHamburgerMenu();
   cartButton();
   createHTML(productList);
-  sortRing();
+  sortItem();
   createFooter();
 };
 let productList: Product[] = products;
@@ -25,31 +25,73 @@ const allProducts: HTMLElement = document.querySelector(
   ".productscontainer"
 ) as HTMLElement;
 
-function sortRing() {
+const allProductsContainer: HTMLDivElement = document.createElement(
+  "div"
+) as HTMLDivElement;
+
+const allProductsFilter: HTMLDivElement = document.createElement("div");
+
+function sortItem() {
+  let allButton: HTMLAnchorElement = document.querySelector(
+    ".filter__all"
+  ) as HTMLAnchorElement;
+
+  allButton.addEventListener("click", () => {
+    createHTML(products);
+  });
+
   let ringButton: HTMLAnchorElement = document.querySelector(
     ".filter__rings"
   ) as HTMLAnchorElement;
 
-  let sortItem = productList.filter(function (products) {
+  let sortItemRings = productList.filter(function (products) {
     return products.type == "Ring";
   });
 
   ringButton.addEventListener("click", () => {
-    createHTML(sortItem);
+    showFilteredProducts(sortItemRings);
   });
 
-  console.log(sortItem);
+  let necklaceButton: HTMLAnchorElement = document.querySelector(
+    ".filter__necklaces"
+  ) as HTMLAnchorElement;
+
+  let sortItemNecklaces = productList.filter(function (products) {
+    return products.type == "Necklace";
+  });
+
+  necklaceButton.addEventListener("click", () => {
+    showFilteredProducts(sortItemNecklaces);
+  });
+
+  let braceletButton: HTMLAnchorElement = document.querySelector(
+    ".filter__bracelets"
+  ) as HTMLAnchorElement;
+
+  let sortItemBracelets = productList.filter(function (products) {
+    return products.type == "Bracelet";
+  });
+
+  braceletButton.addEventListener("click", () => {
+    showFilteredProducts(sortItemBracelets);
+  });
+
+  let earringButton: HTMLAnchorElement = document.querySelector(
+    ".filter__earrings"
+  ) as HTMLAnchorElement;
+
+  let sortItemEarrings = productList.filter(function (products) {
+    return products.type == "Earring";
+  });
+
+  earringButton.addEventListener("click", () => {
+    showFilteredProducts(sortItemEarrings);
+  });
 }
 
-// function sortItems() {
-//   for (let i = 0; i < products.length; i++) {
-//     if (products[i].type === "Ring") {
-//       createHTML();
-//     }
-//   }
-// }
-
 function createHTML(products: Product[]) {
+  allProductsContainer.innerHTML = "";
+  allProductsFilter.innerHTML = "";
   for (let i = 0; i < products.length; i++) {
     const productContainer: HTMLDivElement = document.createElement("div");
     const productImgContainer: HTMLDivElement = document.createElement("div");
@@ -96,107 +138,80 @@ function createHTML(products: Product[]) {
       });
     }
 
-    allProducts.appendChild(productContainer);
+    allProductsContainer.appendChild(productContainer);
     productContainer.appendChild(productImgContainer);
     productImgContainer.appendChild(productImg);
     productImgContainer.appendChild(productBtn);
     productContainer.appendChild(productType);
     productContainer.appendChild(productName);
     productContainer.appendChild(productPrice);
+    allProducts.appendChild(allProductsContainer);
   }
 }
 
-export function filterProducts() {
-  let allButton: HTMLAnchorElement = document.getElementById(
-    "allProductItems"
-  ) as HTMLAnchorElement;
-
-  let ringButton: HTMLAnchorElement = document.getElementById(
-    "ringItem"
-  ) as HTMLAnchorElement;
-
-  let selectedFilter: string = "";
-
-  ringButton.addEventListener("click", () => {
-    let selectedFilter = "All";
-    filterAlternatives(productList, selectedFilter);
-  });
-
-  ringButton.addEventListener("click", () => {
-    console.log("ringar");
-
-    selectedFilter = "Rings";
-    filterAlternatives(productList, selectedFilter);
-  });
-}
-
-function filterAlternatives(products: Product[], selectedFilter: string) {
-  let filteredProductList = products.filter((product) => {
-    return product.type === selectedFilter;
-  });
-  console.log(filteredProductList);
-
-  createHTML(filteredProductList);
-}
-
 const showFilteredProducts = (products: Product[]) => {
-  allProducts.innerHTML = "";
+  allProductsContainer.innerHTML = "";
+  allProductsFilter.innerHTML = "";
 
   for (let i = 0; i < products.length; i++) {
-    for (let i = 0; i < products.length; i++) {
-      const productContainer: HTMLDivElement = document.createElement("div");
-      const productImgContainer: HTMLDivElement = document.createElement("div");
-      const productName: HTMLParagraphElement = document.createElement("p");
-      const productPrice: HTMLParagraphElement = document.createElement("p");
-      const productType: HTMLParagraphElement = document.createElement("p");
-      const productImg: HTMLImageElement = document.createElement("img");
-      const productImgSecond: HTMLImageElement = document.createElement("img");
-      const productBtn: HTMLButtonElement = document.createElement("button");
+    const productContainerFilter: HTMLDivElement =
+      document.createElement("div");
+    const productImgContainerFilter: HTMLDivElement =
+      document.createElement("div");
+    const productNameFilter: HTMLParagraphElement = document.createElement("p");
+    const productPriceFilter: HTMLParagraphElement =
+      document.createElement("p");
+    const productTypeFilter: HTMLParagraphElement = document.createElement("p");
+    const productImgFilter: HTMLImageElement = document.createElement("img");
+    const productImgSecondFilter: HTMLImageElement =
+      document.createElement("img");
+    const productBtnFilter: HTMLButtonElement =
+      document.createElement("button");
 
-      productContainer.classList.add("product");
-      productImgContainer.classList.add("productimgcontainer");
-      productName.classList.add("product__name");
-      productPrice.classList.add("product__price");
-      productType.classList.add("product__type");
-      productImg.classList.add("product__img");
-      productImgSecond.classList.add("product__img--second");
-      productBtn.classList.add("btn");
-      productBtn.classList.add("btn-dark");
-      productBtn.classList.add("product__btn");
+    productContainerFilter.classList.add("product");
+    productImgContainerFilter.classList.add("productimgcontainer");
+    productNameFilter.classList.add("product__name");
+    productPriceFilter.classList.add("product__price");
+    productTypeFilter.classList.add("product__type");
+    productImgFilter.classList.add("product__img");
+    productImgSecondFilter.classList.add("product__img--second");
+    productBtnFilter.classList.add("btn");
+    productBtnFilter.classList.add("btn-dark");
+    productBtnFilter.classList.add("product__btn");
 
-      productName.innerHTML = products[i].name;
-      productPrice.innerHTML = products[i].price.toString() + "kr";
-      productType.innerHTML = products[i].type;
-      productImg.src = products[i].image;
-      productImgSecond.src = products[i].secondImage || productImg.src;
-      productImg.alt = products[i].name;
-      productBtn.innerHTML = `<i class="bi bi-cart-plus-fill"></i>`;
+    productNameFilter.innerHTML = products[i].name;
+    productPriceFilter.innerHTML = products[i].price.toString() + "kr";
+    productTypeFilter.innerHTML = products[i].type;
+    productImgFilter.src = products[i].image;
+    productImgSecondFilter.src =
+      products[i].secondImage || productImgFilter.src;
+    productImgFilter.alt = products[i].name;
+    productBtnFilter.innerHTML = `<i class="bi bi-cart-plus-fill"></i>`;
 
-      productBtn.addEventListener("click", () => {
-        addToCart(products[i]);
+    productBtnFilter.addEventListener("click", () => {
+      addToCart(products[i]);
+    });
+
+    productImgFilter.addEventListener("click", () => {
+      location.href = "../pages/productdetails.html?id=" + products[i].id;
+    });
+
+    if (productImgSecondFilter.src !== productImgFilter.src) {
+      productImgContainerFilter.addEventListener("mouseenter", () => {
+        productImgFilter.src = products[i].secondImage || "";
       });
-
-      productImg.addEventListener("click", () => {
-        location.href = "../pages/productdetails.html?id=" + products[i].id;
+      productImgContainerFilter.addEventListener("mouseleave", () => {
+        productImgFilter.src = products[i].image;
       });
-
-      if (productImgSecond.src !== productImg.src) {
-        productImgContainer.addEventListener("mouseenter", () => {
-          productImg.src = products[i].secondImage || "";
-        });
-        productImgContainer.addEventListener("mouseleave", () => {
-          productImg.src = products[i].image;
-        });
-      }
-
-      allProducts.appendChild(productContainer);
-      productContainer.appendChild(productImgContainer);
-      productImgContainer.appendChild(productImg);
-      productImgContainer.appendChild(productBtn);
-      productContainer.appendChild(productType);
-      productContainer.appendChild(productName);
-      productContainer.appendChild(productPrice);
-      document.body.appendChild(allProducts);
     }
+
+    allProductsFilter.appendChild(productContainerFilter);
+    productContainerFilter.appendChild(productImgContainerFilter);
+    productImgContainerFilter.appendChild(productImgFilter);
+    productImgContainerFilter.appendChild(productBtnFilter);
+    productContainerFilter.appendChild(productTypeFilter);
+    productContainerFilter.appendChild(productNameFilter);
+    productContainerFilter.appendChild(productPriceFilter);
+    allProducts.appendChild(allProductsFilter);
   }
 };
