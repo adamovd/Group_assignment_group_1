3a3d5356d46e35da7c22817aa50491ffefa70800
adamovd <adam.ov.danielsson@gmail.com>
2023-01-02 12:59:31 +0100

    payments options

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -78,3 +78,135 @@ customerForm.addEventListener("submit", (event: SubmitEvent) => {
     customerMail
   );
 });
+
+const paymentContainer: HTMLElement = document.querySelector(
+  ".payment__choice"
+) as HTMLElement;
+const paymentCardRadio: HTMLInputElement = document.getElementById(
+  "credit"
+) as HTMLInputElement;
+const paymentInvoiceRadio: HTMLInputElement = document.getElementById(
+  "invoice"
+) as HTMLInputElement;
+
+paymentCardRadio.type = "radio";
+paymentInvoiceRadio.type = "radio";
+
+paymentCardRadio.addEventListener("click", () => {
+  cardPayment();
+});
+
+paymentInvoiceRadio.addEventListener("click", () => {
+  invoicePayment();
+});
+
+function cardPayment() {
+  paymentContainer.innerHTML = "";
+  const cardNameDiv: HTMLDivElement = document.createElement("div");
+  const cardNumberDiv: HTMLDivElement = document.createElement("div");
+  const cardExpirationDiv: HTMLDivElement = document.createElement("div");
+  const cardCVVDiv: HTMLDivElement = document.createElement("div");
+  const cardNameLabel: HTMLLabelElement = document.createElement("label");
+  const cardNumberLabel: HTMLLabelElement = document.createElement("label");
+  const cardExpirationLabel: HTMLLabelElement = document.createElement("label");
+  const cardCVVLabel: HTMLLabelElement = document.createElement("label");
+  const cardNameInput: HTMLInputElement = document.createElement("input");
+  const cardNumberInput: HTMLInputElement = document.createElement("input");
+  const cardExpirationInput: HTMLInputElement = document.createElement("input");
+  const cardCVVInput: HTMLInputElement = document.createElement("input");
+  const cardNameInvalid: HTMLDivElement = document.createElement("div");
+  const cardNumberInvalid: HTMLDivElement = document.createElement("div");
+  const cardExpirationInvalid: HTMLDivElement = document.createElement("div");
+  const cardCVVInvalid: HTMLDivElement = document.createElement("div");
+
+  cardNameDiv.classList.add("col-md-3");
+  cardNumberDiv.classList.add("col-md-3");
+  cardExpirationDiv.classList.add("col-md-3");
+  cardCVVDiv.classList.add("col-md-3");
+  cardNameLabel.classList.add("form-label");
+  cardNumberLabel.classList.add("form-label");
+  cardExpirationLabel.classList.add("form-label");
+  cardCVVLabel.classList.add("form-label");
+  cardNameInput.classList.add("form-control");
+  cardNumberInput.classList.add("form-control");
+  cardExpirationInput.classList.add("form-control");
+  cardCVVInput.classList.add("form-control");
+  cardNameInvalid.classList.add("invalid-feedback");
+  cardNumberInvalid.classList.add("invalid-feedback");
+  cardExpirationInvalid.classList.add("invalid-feedback");
+  cardCVVInvalid.classList.add("invalid-feedback");
+  cardNameLabel.setAttribute("for", "name");
+  cardNumberLabel.setAttribute("for", "number");
+  cardExpirationLabel.setAttribute("for", "expiration");
+  cardCVVLabel.setAttribute("for", "cvv");
+  cardNameInput.setAttribute("id", "name");
+  cardNumberInput.setAttribute("id", "number");
+  cardExpirationInput.setAttribute("id", "expiration");
+  cardCVVInput.setAttribute("id", "cvv");
+  cardNameInput.type = "text";
+  cardNumberInput.type = "text";
+  cardExpirationInput.type = "text";
+  cardCVVInput.type = "text";
+  cardNameInput.required = true;
+  cardNumberInput.required = true;
+  cardExpirationInput.required = true;
+  cardCVVInput.required = true;
+
+  cardNameLabel.innerHTML = "Name on card";
+  cardNumberLabel.innerHTML = "Card number";
+  cardExpirationLabel.innerHTML = "Expiration";
+  cardCVVLabel.innerHTML = "CVV";
+
+  cardNameDiv.appendChild(cardNameLabel);
+  cardNameDiv.appendChild(cardNameInput);
+  cardNameDiv.appendChild(cardNameInvalid);
+  cardNumberDiv.appendChild(cardNumberLabel);
+  cardNumberDiv.appendChild(cardNumberInput);
+  cardNumberDiv.appendChild(cardNumberInvalid);
+  cardExpirationDiv.appendChild(cardExpirationLabel);
+  cardExpirationDiv.appendChild(cardExpirationInput);
+  cardExpirationDiv.appendChild(cardExpirationInvalid);
+  cardCVVDiv.appendChild(cardCVVLabel);
+  cardCVVDiv.appendChild(cardCVVInput);
+  cardCVVDiv.appendChild(cardCVVInvalid);
+  paymentContainer.appendChild(cardNameDiv);
+  paymentContainer.appendChild(cardNumberDiv);
+  paymentContainer.appendChild(cardExpirationDiv);
+  paymentContainer.appendChild(cardCVVDiv);
+}
+
+function invoicePayment() {
+  paymentContainer.innerHTML = "";
+  const useDeliveryInfoDiv: HTMLDivElement = document.createElement("div");
+  const useDeliveryInfoLabel: HTMLLabelElement =
+    document.createElement("label");
+  const useDeliveryInfoInput: HTMLInputElement =
+    document.createElement("input");
+  const invoiceForm: HTMLFormElement = document.createElement("form");
+  const invoicePnrDiv: HTMLDivElement = document.createElement("div");
+  const invoicePnrLabel: HTMLLabelElement = document.createElement("label");
+  const invoicePnrInput: HTMLInputElement = document.createElement("input");
+
+  useDeliveryInfoDiv.classList.add("form-check");
+  useDeliveryInfoLabel.classList.add("form-check-label");
+  useDeliveryInfoInput.classList.add("form-check-input");
+  useDeliveryInfoInput.setAttribute("id", "info");
+  useDeliveryInfoLabel.setAttribute("for", "info");
+  useDeliveryInfoInput.type = "radio";
+
+  invoiceForm.classList.add("form");
+  invoiceForm.classList.add("invoice__form");
+  invoicePnrDiv.classList.add("invoice__pnr");
+  invoicePnrLabel.classList.add("form-label");
+  invoicePnrInput.classList.add("form-control");
+  invoicePnrLabel.setAttribute("for", "name");
+  invoicePnrInput.setAttribute("id", "name");
+  invoicePnrInput.type = "text";
+  invoicePnrInput.required = true;
+
+  paymentContainer.appendChild(useDeliveryInfoDiv);
+  useDeliveryInfoDiv.appendChild(useDeliveryInfoLabel);
+  useDeliveryInfoDiv.appendChild(useDeliveryInfoInput);
+
+  useDeliveryInfoInput.addEventListener("click", () => {});
+}
