9c3261e6e52fd1b3d174cd369bf3f67e4f4d88fc
h4zan <h4zan@hotmail.com>
2023-01-02 15:01:39 +0100

    message when submiting contact form, changes in contact and about page, prices,removed some text fixed some scss

--- src/ts/models/productList.ts
+++ src/ts/models/productList.ts
@@ -4,7 +4,7 @@ export const products: Product[] = [
   new Product(
     "1",
     "Bespoken",
-    899,
+    15500,
     "https://images.pexels.com/photos/14466168/pexels-photo-14466168.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "A symbol of love and commitment, our beautiful diamond and gold ring speaks elegantly of the love you have for one another. Crafted from 14k gold, this sparkling piece is the perfect token of your love. Delicate gold ring with a delicate but stunning diamond band.",
@@ -15,7 +15,7 @@ export const products: Product[] = [
   new Product(
     "2",
     "Rococo",
-    899,
+    9800,
     "https://images.pexels.com/photos/14466164/pexels-photo-14466164.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "If you're looking for the perfect gift for your loved one, consider a golden ring. This ring is made of pure, natural gold and features a diamond that is cut in an elegant way. The delicate platinum band lets the diamond shine through beautifully.",
@@ -26,7 +26,7 @@ export const products: Product[] = [
   new Product(
     "3",
     "Chatoyant",
-    899,
+    12400,
     "https://images.pexels.com/photos/14466158/pexels-photo-14466158.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "This ring is made of high quality silver and encrusted with real diamonds. The design is glamorous and eye-catching, perfect for any occasion.This diamond ring is crafted from sterling silver and features a cluster of shimmering diamonds. The unique design of this ring is sure to stand out from the crowd. The diamonds are cut to perfection and catch the light beautifully. This ring is the perfect way to add a touch of glamour to any outfit.",
@@ -37,7 +37,7 @@ export const products: Product[] = [
   new Product(
     "4",
     "Zenith",
-    899,
+    2700,
     "https://images.pexels.com/photos/14111399/pexels-photo-14111399.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "This beautiful chain necklace is handcrafted and made of 24K gold. Its curb style design highlights your best assets, while adding a touch of sophistication to any outfit.",
@@ -49,7 +49,7 @@ export const products: Product[] = [
   new Product(
     "5",
     "Redamancy",
-    899,
+    2200,
     "https://images.pexels.com/photos/14111395/pexels-photo-14111395.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "The rosegold chain is a great accessory for any outfit. This piece signifies elegance and timeless style.The rosegold color is perfect for all skin tones and will add a subtle sparkle to your outfit. This necklace is an elegant addition to your jewelry collection.",
@@ -61,7 +61,7 @@ export const products: Product[] = [
   new Product(
     "6",
     "Irenic",
-    899,
+    1890,
     "https://images.pexels.com/photos/14111392/pexels-photo-14111392.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "Reflecting heritage and style, this wide golden chain necklace will add effortless glamour to any outfit. The epitome of sophistication, statement-making and alluring, this golden chain necklace will lend your outfits a touch of exuberant glamour and style.",
@@ -73,7 +73,7 @@ export const products: Product[] = [
   new Product(
     "7",
     "Apricity",
-    899,
+    3200,
     "https://images.pexels.com/photos/10983782/pexels-photo-10983782.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "A necklace for any occasion, this rose gold chain features a heart-shaped diamond pedant in a center setting and is accented with shimmering stones.With its romantic aura, this solid rose-gold chain necklace is a beautiful gift for your loved one.",
@@ -83,7 +83,7 @@ export const products: Product[] = [
   new Product(
     "8",
     "Lucid",
-    899,
+    8200,
     "https://images.pexels.com/photos/10983783/pexels-photo-10983783.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Earring",
     "These spiral shaped earrings are an elegant and timeless accessory. With a diamond dusting across the surface of the earring, each pair is unique and beautiful.A timeless, classic pair of diamond-set earrings, these spirals will effortlessly add sparkle to any outfit.",
@@ -92,7 +92,7 @@ export const products: Product[] = [
   new Product(
     "9",
     "Serenity",
-    899,
+    6400,
     "https://images.pexels.com/photos/10983785/pexels-photo-10983785.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "A timeless piece with a feminine charm. A luxurious chain, adorned with a floral pendant set with diamonds to create the perfect statement necklace. A tasteful and sophisticated necklac, perfect for everyday wear.",
@@ -102,7 +102,7 @@ export const products: Product[] = [
   new Product(
     "10",
     "Apotelesma",
-    899,
+    7999,
     "https://images.pexels.com/photos/10983778/pexels-photo-10983778.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Earring",
     "These teardrop earrings are crafted in rosegold with a dazzling set of diamonds. A timeless piece, they feature a diamond-encrusted crest and finished with invisible posts for pierced ears. Wear them alone or layer them with similar styles from the collection.",
@@ -112,7 +112,7 @@ export const products: Product[] = [
   new Product(
     "11",
     "Esmaralda",
-    899,
+    3400,
     "https://images.pexels.com/photos/10983780/pexels-photo-10983780.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Necklace",
     "This stunning silver necklace with a delicate chain is the perfect accessory for any outfit. Whether you're dressing up for a special occasion or just want to add a touch of glamour to your everyday look, this necklace is a must-have. The dainty chain is adorned with an elegant pendant, making it a subtle and chic addition to any jewelry collection. ",
@@ -122,7 +122,7 @@ export const products: Product[] = [
   new Product(
     "12",
     "Granular",
-    899,
+    8400,
     "https://images.pexels.com/photos/10983781/pexels-photo-10983781.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Earrings",
     "Designed to take a light, these diamond chandelier shaped earrings have double kite-shaped frames that sparkle with a delicate but glamorous intensity. Made of 18-karat gold and featuring pave diamonds, these earrings are the perfect accessory for any occasion.",
@@ -131,7 +131,7 @@ export const products: Product[] = [
   new Product(
     "13",
     "Talisman",
-    899,
+    3600,
     "https://images.pexels.com/photos/10976653/pexels-photo-10976653.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "Wear bold, classic style with these two gold rings. With shimmering diamonds, these rings will add a touch of sophistication to any outfit.These rings are the perfect addtion to your jewelry collection. They feature gold, which is known for its durability and is a great complement to the prominent diamond set in these rings.",
@@ -141,7 +141,7 @@ export const products: Product[] = [
   new Product(
     "14",
     "Ineffable",
-    899,
+    4590,
     "https://images.pexels.com/photos/10976654/pexels-photo-10976654.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Earring",
     "Enchanting, the diamond studs are versatile and chic. Set in 18k gold, pave-set diamonds punctuate a floral design at the center of each earring.These delicate diamond earrings are part of the Dripping in Diamonds collection.",
@@ -151,7 +151,7 @@ export const products: Product[] = [
   new Product(
     "15",
     "Bucolic",
-    899,
+    3800,
     "https://images.pexels.com/photos/10976655/pexels-photo-10976655.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "Dazzle with the classic, vintage-inspired Two Diamond Rose Gold Rings.These two rings in rose gold are a fresh take on the classic, timeless look of diamond. The chic and modern designs of these rings are sure to be a hit at any occasion. Each ring features two sparkling diamonds, which reflect the light back through their prisms.",
@@ -161,7 +161,7 @@ export const products: Product[] = [
   new Product(
     "16",
     "Sempiternal",
-    899,
+    4299,
     "https://images.pexels.com/photos/8891954/pexels-photo-8891954.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Bracelet",
     "A beautiful, must-have braclet duo. The golden bead bracelet goes with everything and the silver bracelet is an updated classic. Wear to the beach or for a night out on the town.Combining two of the most classic pieces of jewellery for a sophisticated look, this braclet is the perfect pairing for your gold and silver collections.",
@@ -171,7 +171,7 @@ export const products: Product[] = [
   new Product(
     "17",
     "Opaline",
-    899,
+    6700,
     "https://images.pexels.com/photos/8891956/pexels-photo-8891956.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Bracelet",
     "A classic diamond bracelet that is made of 14k white gold. The square shaped chain has 40 diamonds total on it, weighing in at a total carat weight of 0.30ctw. Add a touch of shimmer to your everyday style routine with this elegant bracelet. The sleek and sensual outline is crafted from sleek sterling silver, while diamond-cut beads add a glamorous finishing touch.",
@@ -181,7 +181,7 @@ export const products: Product[] = [
   new Product(
     "18",
     "Petrichor",
-    899,
+    3599,
     "https://images.pexels.com/photos/8891950/pexels-photo-8891950.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "Ring",
     " An elegant piece of jewellery that can add a touch of glamour to any outfit.The ring of your dreams: elegant, timeless and ultra-feminine. The ring is made of silver, gold and 6K rose gold.",
@@ -190,7 +190,7 @@ export const products: Product[] = [
   new Product(
     "19",
     "Confluence",
-    899,
+    5800,
     "https://images.pexels.com/photos/8891959/pexels-photo-8891959.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
     "Ring",
     "This striking ring features a silver design with a smooth gold details and sparkling diamonds that create a look of understated yet refined glamour.This piece is full of sparkle and elegance.",
