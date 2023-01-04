5f5efbb727f5ebed71026354d60bddccea087d04
adamovd <adam.ov.danielsson@gmail.com>
2023-01-04 09:04:00 +0100

    startpage carousel

--- src/ts/startpage.ts
+++ src/ts/startpage.ts
@@ -10,8 +10,8 @@ window.addEventListener("load", () => {
   createMenu();
   toggleHamburgerMenu();
   cartButton();
-  // displayStartpage();
-  createPromotion();
+  displayStartpage();
+  // createPromotion();
   createFooter();
   createBadge();
   showProductsStartpage();
@@ -24,18 +24,23 @@ function displayStartpage() {
   const startpageCarousel: HTMLDivElement = document.createElement("div");
   const firstCarouselItem: HTMLDivElement = document.createElement("div");
   const secondCarouselItem: HTMLDivElement = document.createElement("div");
+  const thirdCarouselItem: HTMLDivElement = document.createElement("div");
   const carouselBtnPrev: HTMLButtonElement = document.createElement("button");
   const carouselBtnNext: HTMLButtonElement = document.createElement("button");
   const carouselPrevIcon: HTMLSpanElement = document.createElement("span");
   const carouselNextIcon: HTMLSpanElement = document.createElement("span");
   const startpageFirstImg: HTMLImageElement = document.createElement("img");
   const startpageSecondImg: HTMLImageElement = document.createElement("img");
+  const startpageThirdImg: HTMLImageElement = document.createElement("img");
   const startpageFirstCaption: HTMLDivElement = document.createElement("div");
   const startpageSecondCaption: HTMLDivElement = document.createElement("div");
+  const startpageThirdCaption: HTMLDivElement = document.createElement("div");
   const startpageFirstTitle: HTMLHeadingElement = document.createElement("h3");
   const startpageSecondTitle: HTMLHeadingElement = document.createElement("h3");
+  const startpageThirdTitle: HTMLHeadingElement = document.createElement("h3");
   const startpageFirstText: HTMLSpanElement = document.createElement("span");
   const startpageSecondText: HTMLSpanElement = document.createElement("span");
+  const startpageThirdText: HTMLSpanElement = document.createElement("span");
   const startpageFirstBtn: HTMLButtonElement = document.createElement("button");
   const startpageSecondBtn: HTMLButtonElement =
     document.createElement("button");
@@ -44,6 +49,7 @@ function displayStartpage() {
   firstCarouselItem.classList.add("carousel-item");
   firstCarouselItem.classList.add("active");
   secondCarouselItem.classList.add("carousel-item");
+  thirdCarouselItem.classList.add("carousel-item");
   carouselBtnPrev.classList.add("carousel-control-prev");
   carouselBtnPrev.type = "button";
   carouselBtnPrev.setAttribute("data-bs-target", "#carouselExampleControls");
@@ -57,46 +63,56 @@ function displayStartpage() {
   carouselNextIcon.classList.add("carousel-control-next-icon");
   carouselNextIcon.setAttribute("aria-hidden", "true");
   startpageFirstImg.classList.add("startpage__img");
-  startpageSecondImg.classList.add("startpage__img--first");
-  startpageFirstCaption.classList.add("carousel-caption--second");
+  startpageSecondImg.classList.add("startpage__img");
+  startpageThirdImg.classList.add("startpage__img");
+  startpageFirstCaption.classList.add("carousel-caption");
   startpageSecondCaption.classList.add("carousel-caption");
+  startpageThirdCaption.classList.add("carousel-caption");
   startpageFirstTitle.classList.add("startpage__title");
   startpageSecondTitle.classList.add("startpage__title");
+  startpageThirdTitle.classList.add("startpage__title");
   startpageFirstText.classList.add("startpage__text");
   startpageSecondText.classList.add("startpage__text");
+  startpageThirdText.classList.add("startpage__text");
   startpageFirstBtn.classList.add("startpage__btn");
   startpageSecondBtn.classList.add("startpage__btn");
   startpageFirstBtn.classList.add("btn");
   startpageSecondBtn.classList.add("btn");
+  startpageSecondBtn.classList.add("btn-dark");
   startpageFirstImg.classList.add("d-block");
   startpageSecondImg.classList.add("d-block");
   startpageFirstImg.classList.add("w-100");
   startpageSecondImg.classList.add("w-100");
 
-  startpageFirstImg.src =
-    "https://images.unsplash.com/photo-1591892150204-2f872745bc4b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80";
-  startpageSecondImg.src =
-    "https://images.pexels.com/photos/6408285/pexels-photo-6408285.jpeg";
+  startpageFirstImg.src = "https://i.ibb.co/vmgH7gs/aurora.jpg";
+  startpageSecondImg.src = "https://i.ibb.co/bW14cc6/valentines.jpg";
+  startpageThirdImg.src = "https://i.ibb.co/gWtQRQG/gifts.jpg";
   startpageFirstTitle.innerHTML =
     "Welcome to Aurora Jewelry, the premier destination for exquisite fine jewelry.";
   startpageSecondTitle.innerHTML = `Join us in celebrating love and commitment with our Valentine's Day collection &hearts;`;
-  startpageFirstText.innerHTML = `From timeless classics to unique contemporary designs, our collection is sure to dazzle and delight. Our experienced staff is passionate about helping you find the perfect piece for any occasion. We take pride in our commitment to customer satisfaction and strive to provide exceptional service and quality. Whether you're looking for a special gift, a statement piece, or an heirloom to pass down, Aurora Jewelry has something for everyone. Come visit us today and let us help you find a piece that expresses your personal style and elegance.`;
-  startpageSecondText.innerHTML = `One of the most popular gifts to give on Valentine's Day is jewelry, as it is a timeless and elegant way to show your love. From sparkling diamonds to heartfelt charms, there are endless options to choose from when it comes to finding the perfect piece of jewelry for your loved one.
-  Surprise your loved one with a stunning piece of jewelry from our Valentine's Day collection or treat yourself to something special with our collection of luxury jewelry. From statement necklaces to sparkling gemstone rings, we have something for every occasion and taste.`;
+  startpageFirstText.innerHTML = `From timeless classics to unique contemporary designs, our collection is sure to dazzle and delight.  Aurora Jewelry has something for everyone. Come visit us today and let us help you find a piece that expresses your personal style and elegance.`;
+  startpageSecondText.innerHTML = `One of the most popular gifts to give on Valentine's Day is jewelry, </br>as it is a timeless and elegant way to show your love. From statement necklaces to sparkling gemstone rings, we have something for every occasion and taste.`;
+  startpageThirdTitle.innerHTML = `Sparkle and shine!`;
+  startpageThirdText.innerHTML = `With our dazzling selection of rings!</br> Visit our jewelry shop now for a unique selection of fashionable and timeless rings.`;
 
   startpageContainer.appendChild(startpageCarousel);
   startpageContainer.appendChild(carouselBtnPrev);
   startpageContainer.appendChild(carouselBtnNext);
   startpageCarousel.appendChild(firstCarouselItem);
   startpageCarousel.appendChild(secondCarouselItem);
+  startpageCarousel.appendChild(thirdCarouselItem);
   firstCarouselItem.appendChild(startpageFirstImg);
   firstCarouselItem.appendChild(startpageFirstCaption);
   secondCarouselItem.appendChild(startpageSecondImg);
   secondCarouselItem.appendChild(startpageSecondCaption);
+  thirdCarouselItem.appendChild(startpageThirdImg);
+  thirdCarouselItem.appendChild(startpageThirdCaption);
   startpageFirstCaption.appendChild(startpageFirstTitle);
   startpageFirstCaption.appendChild(startpageFirstText);
   startpageSecondCaption.appendChild(startpageSecondTitle);
   startpageSecondCaption.appendChild(startpageSecondText);
+  startpageThirdCaption.appendChild(startpageThirdTitle);
+  startpageThirdCaption.appendChild(startpageThirdText);
 
   carouselBtnPrev.appendChild(carouselPrevIcon);
   carouselBtnNext.appendChild(carouselNextIcon);
@@ -107,6 +123,18 @@ const divContainer: HTMLDivElement = document.querySelector(
 ) as HTMLDivElement;
 
 function showProductsStartpage() {
+  const spotlightContainer: HTMLDivElement = document.querySelector(
+    ".spotlight"
+  ) as HTMLDivElement;
+  const spotlightTitle: HTMLParagraphElement = document.createElement("p");
+  const spotlightText: HTMLParagraphElement = document.createElement("p");
+  spotlightTitle.classList.add("spotlight__title");
+  spotlightText.classList.add("spotlight__text");
+  spotlightTitle.innerHTML = "In the Spotlight!";
+  spotlightText.innerHTML = "Check out our latest collection.";
+  spotlightContainer.appendChild(spotlightTitle);
+  spotlightContainer.appendChild(spotlightText);
+  document.body.appendChild(spotlightContainer);
   let productFilter = products.splice(15, 15);
 
   for (let i = 0; i < productFilter.length; i++) {
